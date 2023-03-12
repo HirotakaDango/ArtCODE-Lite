223e4e11594cd61a2bac1bb5c672f144b7b5eed3
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-13 01:50:18 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<center style="margin-bottom: 60px; font-weight: 800; color: gray;">
+  <nav class="navbar fixed-top bg-light shadow" style="padding-bottom: 7px; padding-top: 7px;">
+    <div class="bb1 container">
+      <a class="nav-link px-2 text-secondary" href="imgupload.php"><i class="bi bi-cloud-arrow-up-fill"></i></i></a>
+      <h1 style="color: gray; margin-top: 7px;" class="nav-link px-2 text-secondary"><a class="nav-link border-bottom" href="index.php">ArtCODE LITE</a></h1>
+      <div class="dropdown">
+        <a class="nav-link text-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-person-circle"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+          <li><a class="dropdown-item text-secondary fw-bold" href="profile.php">Profile</a></li>
+          <li><a class="dropdown-item text-secondary fw-bold" href="logout.php">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</center>
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header("Location: session.php");
+    exit;
+  }
+
+  // Connect to the SQLite database
+  $db = new SQLite3('database.sqlite');
+  $db->exec("CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, filename TEXT, username TEXT)");
+
+  // Get all of the images from the database
+  $result = $db->query("SELECT * FROM images ORDER BY id DESC");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <style>
+      .images {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        grid-gap: 2px;
+        justify-content: center;
+        margin-right: 3px;
+        margin-left: 3px;
+      }
+ 
+      .images a {
+        display: block;
+        border-radius: 4px;
+        overflow: hidden;
+        border: 2px solid #ccc;
+    }
+ 
+    .images img {
+        width: 100%;
+        height: 150px;
+        object-fit: cover;
+            }
+
+    .btn1 {
+        padding: 10px;
+        margin: 10px; 
+        border: 8px solid #eee;
+        border-radius: 15px;
+        color: gray;
+        font-weight: 700;
+        padding: 8px 20px;
+    }
+
+  </style>
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <div class="images"><?php while ($image = $result->fetchArray()): ?>
+      <a href="images/<?php echo $image['filename']; ?>">
+        <img src="thumbnails/<?php echo $image['filename']; ?>">
+      </a>
+    <?php endwhile; ?></div>
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</html>
